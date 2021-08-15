cd9e401b04cbe7d0ab859ee9d301addf573d0092
mihail.uzunov <m.uzunov1914@gmail.com>
2021-08-15 14:13:20 +0300

    6 list basic

---
+++ 6.py
@@ -0,0 +1,17 @@
+referee_book = input().split(" ")
+
+count_A = 11
+count_B = 11
+referee_book_without_dublicates = list(set(referee_book))
+for player in referee_book_without_dublicates:
+    player = player.split("-")
+    if player[0] == "A":
+        count_A -= 1
+    else:
+        count_B -= 1
+print(f"Team A - {count_A}; Team B - {count_B}")
+if count_A < 7 or count_B < 7:
+    print("Game was terminated")
+
+
+
